7052baae3e1de9d0c7397048f8360df520df3b2c
programrsky <lovesky00317@gmail.com>
2024-04-03 14:25:52 +0900

    a

--- test-app/src/index.js
+++ test-app/src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Welcome from './ch_03/wel';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <div>
+        <Welcome name="하늘" />
+        <Welcome name="sky" />
+        <Welcome name="Leesky" />
+    </div>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
