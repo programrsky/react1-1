6984eb7e34e72429b6da55501893ef86b579136b
programrsky <lovesky00317@gmail.com>
2024-04-23 22:53:38 +0900

    Update Book.jsx

--- test-app/src/ch05/Book.jsx
+++ test-app/src/ch05/Book.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Book(props) {
     return (
-        <div>
+        <div>asd
             <h1>{`이책의 이름은 ${props.name}입니다.`}</h1>
             <h1>{`이책은 총 ${props.numOfPage}페이지로 이뤄져 있습니다.`}</h1>
         </div>
